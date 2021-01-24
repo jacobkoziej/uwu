9eb8fecb4e01ade743966135216db2f9a612bd19
Jacob Koziej <jacobkoziej@gmail.com>
2021-01-24 00:42:39 -0500

    Bump Copyright year to 2021

--- src/config.c
+++ src/config.c
@@ -1,6 +1,6 @@
 /*
  * config.c -- configuration of the uwu program
- * Copyright (C) 2020  Jacob Koziej <jacobkoziej@gmail.com>
+ * Copyright (C) 2020-2021  Jacob Koziej <jacobkoziej@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
